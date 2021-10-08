f883242c24f45813613a9fde54fd6605b3710efc
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-10-08 23:10:45 +0530

    Class and Object

---
+++ OOPS_CONCEPT/Area_of_Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOPS_CONCEPT
+{
+    class Area_of_Rectangle
+    {
+        double length;
+        double width;
+        public void Rectangle()
+        {
+            length = 10;
+            width = 20;
+        }
+        public double Area()
+        {
+             return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length:", length);
+            Console.WriteLine("Width:", width);
+            Console.WriteLine("Area:", Area());
+        }
+    }
+}
---
+++ OOPS_CONCEPT/Output.cs
@@ -0,0 +1,19 @@
+﻿using OOPS_CONCEPT;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_CONCEPT
+{
+    class Output
+    {
+        static void Main(string[] argsS)
+        {
+            Area_of_Rectangle r = new Area_of_Rectangle();
+            r.Area();
+            r.Display();
+        }
+        
+    }
+}
