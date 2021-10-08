c5f1f1ef7cc4f51e2059fdff5b9ffb96f8e5e33d
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-10-08 23:33:26 +0530

    Abstract

--- OOPS_CONCEPT/Area_of_Rectangle.cs
+++ OOPS_CONCEPT/Area_of_Rectangle.cs
@@ -2,25 +2,52 @@
 
 namespace OOPS_CONCEPT
 {
-    class Area_of_Rectangle
+    public abstract class Area_of_Rectangle
     {
-        double length;
-        double width;
-        public void Rectangle()
+        public double length = 10;
+        public double width = 20;
+        public abstract void Area();
+    }
+    public class Rect1 : Area_of_Rectangle
+    {
+        public override void Area()
         {
-            length = 10;
-            width = 20;
-        }
-        public double Area()
-        {
-             return length * width;
+            Console.WriteLine("Length:" + length);
         }
 
-        public void Display()
+    }
+
+    public class Rect2 : Area_of_Rectangle
+    {
+        public override void Area()
         {
-            Console.WriteLine("Length:", length);
-            Console.WriteLine("Width:", width);
-            Console.WriteLine("Area:", Area());
+            Console.WriteLine("Width:" + width);
         }
+
+    }
+
+    public class Are_Rect : Area_of_Rectangle
+    {
+        public override void Area()
+        {
+            double a = length * width;
+            Console.WriteLine("Area:" + a);
+        }
+
+    }
+
+    public class area_rect
+    {
+        public static void Main()
+        {
+            Area_of_Rectangle r;
+            r = new Rect1();
+            r.Area();
+            r = new Rect2();
+            r.Area();
+            r = new Are_Rect();
+            r.Area();
+        }
+           
     }
 }
